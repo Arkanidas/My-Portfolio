afd6a1dba6a4f9f56189461752590af8b9d180bf
Leon <Leon>
2024-02-08 03:19:57 +0100

    Portfolio beginning

---
+++ Portfolio/src/Landing.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import '../src/styles/atropos.css'
+
+function Landing() {
+
+
+  return (
+    <>
+     
+<h1>Jesus</h1>
+
+
+
+
+    </>
+  )
+}
+
+export default Landing
