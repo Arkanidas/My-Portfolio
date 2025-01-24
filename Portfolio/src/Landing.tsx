901aa1c2f607750b709c69069d423a56ddc4b6fa
Leon <xleonidasprox@gmail.com>
2025-01-24 21:56:06 +1300

    center info text

--- Portfolio/src/Landing.tsx
+++ Portfolio/src/Landing.tsx
@@ -1,21 +1,27 @@
 import { useState } from 'react';
 import Navigation from '../src/components/Navigation';
 import {AnimatePresence, motion} from 'framer-motion'
-
+import "./styles/styles.css"
 
  function Landing() {
 
 
   return (
+    <div>
     <div className='Landing'>
     <Navigation/>
+
     
- 
- 
+    <div className='info_container'>
+    <h1>Leon Alexander Aysa</h1>
+    <h3>Hi I'm a Frontend Developer</h3>
+
+    </div>
+
+    </div>
 
 
-
-  
+   
     </div>
   )
 }
