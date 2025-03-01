d5970a4e6dcb398a0fc6d490fca38759b383bca7
Leon <xleonidasprox@gmail.com>
2025-03-01 15:24:45 +1300

    contactform + emailjs added

--- Portfolio/src/components/Contactform.tsx
+++ Portfolio/src/components/Contactform.tsx
@@ -2,10 +2,9 @@ import React from 'react'
 import "../styles/contactstyle.css"
 
 
-export default function Contact() {
+export default function Contactform() {
   return (
     <div className='Contact'>
-      <h1 className='title_c'>Contact me</h1>
       <div className='contact-container'>
 
     
