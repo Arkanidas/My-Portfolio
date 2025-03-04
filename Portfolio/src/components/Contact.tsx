5e09322d26a45c9b39ddd7df1c538afe77b0867a
Leon <xleonidasprox@gmail.com>
2025-03-04 21:24:33 +1300

    deleted images + fixed help symbol and colors

--- Portfolio/src/components/Contact.tsx
+++ Portfolio/src/components/Contact.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import "../styles/contactstyle.css"
 import Contactform from './Contactform'
 
--- Portfolio/src/components/Contactform.tsx
+++ Portfolio/src/components/Contactform.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
 import "../styles/contactstyle.css"
-
+import { RxQuestionMarkCircled } from "react-icons/rx";
 
 export default function Contactform() {
   return (
   <div>
    
    <form className="form">
-      <div className="title-2">Send me an Email!</div>
- 
+
+    <div className="header-container">
+      <div className="title-2">Send me an Email! </div>
+
+      <div className="tooltip-container">
+      <div className="tooltip">Uiverse.io</div>
+      <RxQuestionMarkCircled className="questionmark" style={{width:"30px", height:"30px"}}/>
+</div>
+</div>
+
+
       <label className="fname">Your name:</label>
       <div className="input-container">
  
@@ -20,20 +28,21 @@ export default function Contactform() {
         <span className="star"></span>
         <span className="star"></span>
         <span className="star"></span>
+        <span className="star"></span>
       </section>
 
       <label className="fname">Your Mail:</label>
       <div className="input-container">
-        <input className="input-mail" type="mail" placeholder="Enter Your Mail"></input>
+        <input className="input-mail" type="text" placeholder="Enter Your Mail"></input>
       </div>
 
       <label className="fname">Your Message:</label>
       <div className="input-container">
-      <textarea  className="input-message" name="message" placeholder='Enter Your Message'></textarea>
+      <textarea className="input-message" name="message" placeholder='Enter Your Message'></textarea>
       </div>
 
       <button type="submit" className="submit">
-        <div className="sign-text">Sign in</div>
+        <div className="sign-text">Send</div>
       </button>
 
      
