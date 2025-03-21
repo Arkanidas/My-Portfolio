75813ee796d9ffafb7a6c3a52e9daae80fad1304
Leon <xleonidasprox@gmail.com>
2025-03-21 20:11:22 +1300

    background and cards fixed and improved

--- Portfolio/src/components/Projects.tsx
+++ Portfolio/src/components/Projects.tsx
@@ -2,6 +2,8 @@ import Navigation from "./Navigation"
 import "../styles/Projects.css"
 import { motion } from "framer-motion";
 import pro from "../images/pro1.jpg"
+import bracket from "../images/brackets.png"
+
 
 const containerVariants = {
   hidden: { opacity: 0,},
@@ -22,8 +24,8 @@ const childVariants = {
 
 const myprojects = [
   {
-    title: "Project 1",
-    description: "This is a description of project 1.",
+    title: "Rock, Paper & Scissor",
+    description: "This is a classic Rock, Paper & Scissor game. It is built using HTML, CSS and Javascript. Battle against the computer and see who wins.",
     img: pro, 
     link_prev: "#",
     link_github: "#",
@@ -50,11 +52,25 @@ export default function Projects() {
     className="Projects"
     initial="hidden"
     animate="visible"
-    variants={containerVariants}
+  
   >
+      <div className="particle-container">
+    <div className="particle"></div>
+    <div className="particle"></div>
+    <div className="particle"></div>
+    <div className="particle"></div>
+    <div className="particle"></div>
+    <div className="particle"></div>
+    <div className="particle"></div>
+    <div className="particle"></div>
+    <div className="particle"></div>
+    <div className="particle"></div>
+  </div>
+
       <Navigation/>
       <div className="description-container">
       <h2 className="project-header">My Projects</h2>
+     
       <p className="descriptions">Here are my Projects that I've been working with. Feel free to explore or send me suggestions for improvements</p>
       
       </div>
@@ -62,8 +78,19 @@ export default function Projects() {
       <motion.div className="project-container" variants={containerVariants}>
         <motion.div className="project-child" variants={childVariants}>
           <img src={myprojects[0].img} alt="Project 1" className="project-image" />
-          <h2 className="project-title">Rock, Paper & Scissor</h2>
-        
+          <h2 className="project-title">{myprojects[0].title}</h2>
+          <div className="skill-container">
+            <div className="skill" style={{ backgroundColor: "#E3552B" }}>Html</div>
+            <div className="skill" style={{ backgroundColor: "#1B75B8" }}>CSS</div>
+            <div className="skill" style={{ backgroundColor: "#F7E025", color:"black" }}>Javascript</div>
+          </div>
+
+          <p className="project-description">{myprojects[0].description}</p>
+
+          <div className="button-container">
+         <div className="github-button"><img src={bracket} style={{width:"30px", height:"30px", marginTop:"10%"}}></img></div>
+         <div className="prev-button">Live Preview <div className="live-icon"></div></div>
+          </div>
         </motion.div>
 
         <motion.div className="project-child" variants={childVariants}>
