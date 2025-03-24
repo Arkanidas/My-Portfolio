65f26a32f26df82e08b9e09c72b7313a882f8880
Leon <xleonidasprox@gmail.com>
2025-03-24 23:02:00 +1300

    buttons, projects & other adjustments made

--- Portfolio/src/components/Projects.tsx
+++ Portfolio/src/components/Projects.tsx
@@ -1,28 +1,37 @@
 import Navigation from "./Navigation"
 import "../styles/Projects.css"
-import { motion } from "framer-motion";
+import { motion} from "framer-motion";
 import pro from "../images/pro1.jpg"
 import bracket from "../images/brackets.png"
+import bildhistoria2 from "../images/bildhistoria2.png"
 
+interface Project {
+  title: string;
+  description: string;
+  img: string;
+  link_prev: string;
+  link_github: string;
+}
 
 const containerVariants = {
   hidden: { opacity: 0,},
-  
   visible: {
     opacity: 1,
     transition: {
-      staggerChildren: 0.1, 
-      duration:0.1,
+      staggerChildren: 0.5, 
+      duration:1,
     },
   },
 };
 
 const childVariants = {
-  hidden: { opacity: 0, y: 50 },
+  hidden: { opacity: 0, y: 150},
   visible: { opacity: 1, y: 0, transition: { duration: 0.6, ease: "easeOut" } },
 };
 
-const myprojects = [
+
+
+const myprojects:Project[] = [
   {
     title: "Rock, Paper & Scissor",
     description: "This is a classic Rock, Paper & Scissor game. It is built using HTML, CSS and Javascript. Battle against the computer and see who wins.",
@@ -31,16 +40,16 @@ const myprojects = [
     link_github: "#",
   },
   {
-    title2: "Project 2",
-    description2: "This is a description of project 2.",
-    img2: "https://via.placeholder.com/400x250",
-    link_prev: "#",
-    link_github: "#",
+    title: "Bildhistoria",
+    description: "Bildhistoria - Sweden's largest place for copyright free historical photographs. Together with backend, frontend and cloud developers we have created Bildhistoria for everyone to use & explore ",
+    img: bildhistoria2,
+    link_prev: "https://bildhistoria.se/",
+    link_github: "https://github.com/svenskt-portrattarkiv/bildhistoria",
   },
   {
-    title3: "Project 3",
-    description3: "This is a description of project 3.",
-    img3: "https://via.placeholder.com/400x250",
+    title: "IMDB Api Project",
+    description: "This is a description of project 3.",
+    img: "https://via.placeholder.com/400x250",
     link_prev: "#",
     link_github: "#",
   },
@@ -55,7 +64,6 @@ export default function Projects({count = 50}) {
   
   >
      
-
       <Navigation/>
       <div className="description-container">
       <h2 className="project-header">My Projects</h2>
@@ -67,8 +75,8 @@ export default function Projects({count = 50}) {
       <motion.div className="project-container" variants={containerVariants}>
       <div className="particle-container">
   {Array.from({ length: count }).map((_, index) => {
-    const duration = Math.random() * (50 - 30) + 30; 
-    const delay = Math.random() * 5; 
+    const duration = Math.random() * (50 - 30) + 50; 
+    const delay = Math.random() * 3; 
 
 
     return (
@@ -84,11 +92,11 @@ export default function Projects({count = 50}) {
     );
   })}
 </div>
-        <motion.div className="project-child" variants={childVariants}>
+        <motion.div className="project-child" variants={childVariants}  whileHover={{ scale: 1.05 }}>
           <img src={myprojects[0].img} alt="Project 1" className="project-image" />
           <h2 className="project-title">{myprojects[0].title}</h2>
           <div className="skill-container">
-            <div className="skill" style={{ backgroundColor: "#E3552B" }}>Html</div>
+            <div className="skill" style={{ backgroundColor: "#E3552B" }}>HTML</div>
             <div className="skill" style={{ backgroundColor: "#1B75B8" }}>CSS</div>
             <div className="skill" style={{ backgroundColor: "#F7E025", color:"black" }}>Javascript</div>
           </div>
@@ -96,19 +104,47 @@ export default function Projects({count = 50}) {
           <p className="project-description">{myprojects[0].description}</p>
 
           <div className="button-container">
+         <a href={myprojects[1].link_github}>
          <div className="github-button"><img src={bracket} style={{width:"30px", height:"30px", marginTop:"10%"}}></img></div>
-         <div className="prev-button">Live Preview <div className="live-icon"></div></div>
-          </div>
-        </motion.div>
+         </a>
 
-        <motion.div className="project-child" variants={childVariants}>
-        <img src={myprojects[0].img} alt="Project 1" className="project-image" />
+          <a href={myprojects[1].link_prev}>
+         <div className="prev-button">Live Preview <div className="live-icon"></div></div>
+         </a>
+         
+         </div>
          
         </motion.div>
 
-        <motion.div className="project-child" variants={childVariants}>
+
+        
+        <motion.div className="project-child" variants={childVariants}  whileHover={{ scale: 1.05 }}>
+        <img src={myprojects[1].img} style={{}}alt="Project 1" className="project-image" />
+          <h2 className="project-title">{myprojects[1].title}</h2>
+          <div className="skill-container">
+            <div className="skill" style={{ backgroundColor: "#087ECE" }}>TypeScript</div>
+            <div className="skill" style={{ backgroundColor: "#85CCD7", color:"black" }}>React</div>
+            <div className="skill" style={{ backgroundColor: "#8A18FD", color:"white" }}>Bootstrap</div>
+            <div className="skill" style={{ backgroundColor: "none", color:"white", border:"1px solid black" }}>Docker</div>
+          </div>
+
+          <p className="project-description">{myprojects[1].description}</p>
+
+          <div className="button-container">
+          <a href={myprojects[1].link_github}>
+         <div className="github-button"><img src={bracket} style={{width:"30px", height:"30px", marginTop:"10%"}}></img></div>
+         </a>
+               
+          <a href={myprojects[1].link_prev}>
+         <div className="prev-button">Live Preview <div className="live-icon"></div></div>
+         </a>
+          </div>
+        </motion.div>
+
+        <motion.div className="project-child" variants={childVariants}  whileHover={{ scale: 1.05 }}>
         <img src={myprojects[0].img} alt="Project 1" className="project-image" />
         </motion.div>
+
       </motion.div>
     </motion.div>
    
